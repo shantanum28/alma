f6b6e467748d621ce06fc32cdd1064cd8287d6db
Shantanu Meshram <smeshram1@hawk.iit.edu>
2024-02-14 14:38:03 -0600

    Encoders 2

--- smeshram1/itmd-521/labs/week-05/scala/src/main/scala/assignment_02/assignment_02.scala
+++ smeshram1/itmd-521/labs/week-05/scala/src/main/scala/assignment_02/assignment_02.scala
@@ -1,4 +1,4 @@
-import org.apache.spark.sql.{SparkSession, functions}
+import org.apache.spark.sql.{SparkSession, functions, Encoders}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
