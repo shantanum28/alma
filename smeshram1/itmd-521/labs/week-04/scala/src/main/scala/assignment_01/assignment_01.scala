3425e934bac28c192166d1cd52db6c190a67dd7f
Shantanu Meshram <smeshram1@hawk.iit.edu>
2024-03-20 21:49:33 -0500

    asdfdsa456c56sdr1fg645dsa4

--- smeshram1/itmd-521/labs/week-04/scala/src/main/scala/assignment_01/assignment_01.scala
+++ smeshram1/itmd-521/labs/week-04/scala/src/main/scala/assignment_01/assignment_01.scala
@@ -1,7 +1,7 @@
 import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType}
 
-object Assignment01 {
+object assignment_01 {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder
       .master("local[*]") 
