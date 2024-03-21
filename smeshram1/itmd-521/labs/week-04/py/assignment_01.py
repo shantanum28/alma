0c7ec508f47d2292984097c35b6766461fe4f8da
Shantanu Meshram <smeshram1@hawk.iit.edu>
2024-03-20 23:00:07 -0500

    5sd6a4f65cs156dsf4g894dfsg651v265sf4gd5

--- smeshram1/itmd-521/labs/week-04/py/assignment_01.py
+++ smeshram1/itmd-521/labs/week-04/py/assignment_01.py
@@ -1,55 +1,58 @@
+#First infer the schema and read the csv file
 import sys
-
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import count
 from pyspark.sql.types import *
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: DivvyTrips <file>", file=sys.stderr)
+      if len(sys.argv) != 2:
+        print("Usage: mnmcount <file>", file=sys.stderr)
         sys.exit(-1)
-    
-    spark = (SparkSession
-    .builder
-    .appName("Assignment1")
-    .getOrCreate())
-    Divvy_File = sys.argv[1]
-    
-    DataFrame_CSV = (spark.read.format("csv")
-    .option("header","true")
-    .option("inferSchema", "true")
-    .load(Divvy_File))
-    DataFrame_CSV.show(n=5, truncate=False)
-    DataFrame_CSV.printSchema()
-    #parquet_path = DataFrame_CSV.write.format("parquet").save("/home/vagrant/gdisale/assignment-01/example-data/Divvy-2015")
-    Parquert_File = spark.read.format("parquet").load("/home/vagrant/gdisale/assignment-01/example-data/Divvy-2015")
-    Parquert_File.show(n=5, truncate=False)
+      spark = (SparkSession
+      .builder
+      .appName("Divya_Trips")
+      .getOrCreate())
+      fire_schema = StructType([StructField('CallNumber', IntegerType(), True),
+    StructField('UnitID', StringType(), True),
+    StructField('IncidentNumber', IntegerType(), True),
+    StructField('CallType', StringType(), True),
+    StructField('CallDate', StringType(), True),
+    StructField('WatchDate', StringType(), True),
+    StructField('CallFinalDisposition', StringType(), True),
+    StructField('AvailableDtTm', StringType(), True),
+    StructField('Address', StringType(), True),
+    StructField('City', StringType(), True),
+    StructField('Zipcode', IntegerType(), True),
+    StructField('Battalion', StringType(), True),
+    StructField('StationArea', StringType(), True),
+    StructField('Box', StringType(), True),
+    StructField('OriginalPriority', StringType(), True),
+    StructField('Priority', StringType(), True),
+    StructField('FinalPriority', IntegerType(), True),
+    StructField('ALSUnit', BooleanType(), True),
+    StructField('CallTypeGroup', StringType(), True),
+    StructField('NumAlarms', IntegerType(), True),
+    StructField('UnitType', StringType(), True),
+    StructField('UnitSequenceInCallDispatch', IntegerType(), True),
+    StructField('FirePreventionDistrict', StringType(), True),
+    StructField('SupervisorDistrict', StringType(), True),
+    StructField('Neighborhood', StringType(), True),
+    StructField('Location', StringType(), True),
+    StructField('RowID', StringType(), True),
+    StructField('Delay', FloatType(), True)])
+      csv_file = sys.argv[1]
+      sampleDF = spark.read.option("samplingRatio", 0.001) .option("header", True) .csv(csv_file)
+      print(sampleDF.printSchema())
 
 
-    ## Using StructType below
-
-    gSchema = StructType([StructField("trip_id", IntegerType()),
-      StructField("starttime", StringType()),
-      StructField("stoptime", StringType()),
-      StructField("bikeid", IntegerType()),
-      StructField("tripduration", IntegerType()),
-      StructField("from_station_id", IntegerType()),
-      StructField("from_station_name", StringType()),
-      StructField("to_station_id", IntegerType()),
-      StructField("to_station_name", StringType()),
-      StructField("usertype", StringType()),
-      StructField("gender", StringType()),
-      StructField("birthyear", IntegerType())])
-    
-    Structure_DataFrame = (spark.read.schema(gSchema).format("csv")).option("header", "true").option("structureSchema", "true").load(Divvy_File)
-    Structure_DataFrame.show()
-    Structure_DataFrame.printSchema()
-
-    ## using Data Definition Language 
-
-    data_gschema = "trip_id INT,starttime STRING,stoptime STRING,bikeid INT,tripduration INT,from_station_id INT,from_station_name STRING,to_station_id INT,to_station_name STRING,usertype STRING,gender STRING,birthyear INT"
-    blogsDataFrame = (spark.read.schema(data_gschema).format("csv")).option("header", "true").load(Divvy_File)
-    blogsDataFrame.show()
-    blogsDataFrame.printSchema()
-    gender_dataframe = (DataFrame_CSV.select("gender", "to_station_id", "to_station_name").where(DataFrame_CSV.gender == 'Female').groupBy("to_station_id").count())
-    gender_dataframe.show(n=10, truncate=False)
+      schema2 = "trip_id STRING, starttime STRING, stoptime STRING, bikeid STRING, tripduration STRING, from_station_id STRING, from_station_name STRING, to_station_id STRING, to_station_name STRING, usertype STRING, gender STRING, birthyear STRING"
+      csv_file = sys.argv[1]
+      sampleDF = spark.read.option("samplingRatio", 0.001) .option("header", True) .csv(csv_file)
+      data_frame=spark.read.csv(csv_file, header=True, schema=schema)
+      data_frame2=spark.read.csv(csv_file, header=True, schema=schema2)
+      print(sampleDF.printSchema())
+      print(sampleDF.count())
+      print(data_frame.printSchema())
+      print(data_frame.count())
+      print(data_frame2.printSchema())
+      print(data_frame2.count())
+      final_data_frame = spark .read.option("samplingRatio", 0.001) .option("header", True) .csv(csv_file)
+      (final_data_frame.select("gender","to_station_name").where(final_data_frame.gender=="Female").groupBy("gender","to_station_name").count().show(10,truncate=False))
--- smeshram1/itmd-521/labs/week-04/scala/src/main/scala/assignment_01/assignment_01.scala
+++ smeshram1/itmd-521/labs/week-04/scala/src/main/scala/assignment_01/assignment_01.scala
@@ -1,52 +1,46 @@
-package main.scala.assignment
+package main.scala.assignment_01
+import org.apache.spark.sql.functions.avg 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
-//import sys._
+object assignment_01 {
+  def main(args: Array[String]): Unit = { 
+val spark = SparkSession
+      .builder
+      .appName("AuthorsAges")
+      .getOrCreate()
 
-/**
- * Usage: MnMcount <mnm_file_dataset>
- */
-object assignment{
-def main(args: Array[String]) {
-     val spark = SparkSession.builder.appName("Assignment1").getOrCreate()
-     if (args.length < 1) {
-     print("Usage: DivvySet <Divvy_file_dataset>") 
-     sys.exit(1)
-     }
+//Infer schema
+val csv_file = args(0)
+val sampleDF = spark .read.option("samplingRatio", 0.001).option("header", true) .csv(csv_file)
+print(sampleDF.printSchema())
+print(sampleDF.count())
 
-     val Divvy_File = args(0)
-     val Infer_DataFrame = spark.read.format("csv") .option("header", "true") .option("inferSchema", "true") .load(Divvy_File)
-     Infer_DataFrame.show(20)
-     Infer_DataFrame.printSchema()
+//structfields
+val schema = StructType(Array(StructField("trip_id", StringType, true),
+                            StructField("starttime", StringType, true),
+                            StructField("stoptime", StringType, true),
+                            StructField("bikeid", StringType, true),
+                            StructField("tripduration", StringType, true),
+                            StructField("from_station_id", StringType, true),
+                            StructField("from_station_name", StringType, true),
+                            StructField("to_station_id", StringType, true),
+                            StructField("to_station_name", StringType, true),
+                            StructField("usertype", StringType, true),
+                            StructField("gender", StringType, true),
+                            StructField("birthyear", StringType, true)))
+val data_frame = spark.read.schema(schema).option("header", "true").csv(csv_file)
+print(data_frame.printSchema())
+print(data_frame.count())
 
+//ddl
+var schema2 = "trip_id STRING, starttime STRING, stoptime STRING, bikeid STRING, tripduration STRING, from_station_id STRING, from_station_name STRING, to_station_id STRING, to_station_name STRING, usertype STRING, gender STRING, birthyear STRING"
+val data_frame2 = spark.read.schema(schema2).option("header", "true").csv(csv_file)
+println(data_frame2.printSchema())
+print(data_frame2.count())
 
-     val Schema = StructType(Array(StructField("trip_id", IntegerType),
-     StructField("starttime", StringType),
-     StructField("stoptime", StringType),
-     StructField("bikeid", IntegerType),
-     StructField("tripduration", IntegerType),
-     StructField("from_station_id", IntegerType),
-     StructField("from_station_name", StringType),
-     StructField("to_station_id", IntegerType),
-     StructField("to_station_name", StringType),
-     StructField("usertype", StringType),
-     StructField("gender", StringType),
-     StructField("birthyear", IntegerType)))
+//final
+val data_frame3 = spark .read.option("samplingRatio", 0.001) .option("header", true) .csv(csv_file)
+(data_frame3.select("gender","to_station_name").where(data_frame3("gender")==="Female").groupBy("gender","to_station_name").count().show(10,false))
 
-     val Struct_DataFrame = spark.read.schema(Schema).format("csv").option("header", "true").option("structureSchema", "true").load(Divvy_File)
-     Struct_DataFrame.show(10)
-     Struct_DataFrame.printSchema()
-
-// DDl Schema
-     val Data_schema = "trip_id INT,starttime STRING,stoptime STRING,bikeid INT,tripduration INT,from_station_id INT,from_station_name STRING,to_station_id INT,to_station_name STRING,usertype STRING,gender STRING,birthyear INT"
-     val BlogsDataFrame = (spark.read.schema(Data_schema).format("csv")).option("header", "true").load(Divvy_File)
-     BlogsDataFrame.show()
-     BlogsDataFrame.printSchema()
-
-     // Transformations and actions
-     val Gender_DataFrame=(Infer_DataFrame.select("gender", "to_station_id", "to_station_name").where(Infer_DataFrame.gender == "Female"))
-     Gender_DataFrame.groupBy("to_station_id").count().show(10)
-
-}
+  }
 }
